fa119783ea5a06eb42e5c3ccee443d6efdf6e896
AgRodriguezC <ag.rodriguez@duocuc.cl>
2023-04-14 19:03:40 -0400

    Primer commit Proyecto

---
+++ changelog.md
@@ -0,0 +1,2 @@
+# Changelog
+- Aquí irá el registro de cambios, commits, todo's con fecha y descripción.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
+import { StartComponent } from './pages/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent,
+    LoginAdminComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
