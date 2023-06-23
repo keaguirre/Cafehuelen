367e05817331a7f25b38e5f79057e174e5e688cb
badbugui <be.bugueno@duocuc.cl>
2023-06-23 14:16:27 -0400

    Actualización fuentes para legibilidad en boleta, pequeños ajustes par el silentprint.

--- src/app/components/print/print.component.ts
+++ src/app/components/print/print.component.ts
@@ -4,117 +4,127 @@ import { CarritoService } from 'src/app/services/carritoService/carrito.service'
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
 import * as printJS from 'print-js';
+import { style } from '@angular/animations';
 
 @Component({
-  selector: 'app-print',
-  templateUrl: './print.component.html',
-  styleUrls: ['./print.component.css']
+    selector: 'app-print',
+    templateUrl: './print.component.html',
+    styleUrls: ['./print.component.css'],
 })
 export class PrintComponent implements OnInit {
-  orderCarrito: any;
-  itemsCarrito: any;
-  infoBack: any;
-  infoQr: string = '';
+    orderCarrito: any;
+    itemsCarrito: any;
+    infoBack: any;
+    infoQr: string = '';
 
-  constructor(
-    private _route: ActivatedRoute,
-    private carritoService: CarritoService,
-    private elementRef: ElementRef
-  ) {
-    this.infoQr = JSON.stringify(this.infoBack);
-    this.infoBack;
-  }
+    constructor(
+        private _route: ActivatedRoute,
+        private carritoService: CarritoService,
+        private elementRef: ElementRef
+    ) {
+        this.infoQr = JSON.stringify(this.infoBack);
+        this.infoBack;
+    }
 
-  ngOnInit(): void {
-    this._route.queryParams.subscribe(params => {
-      this.orderCarrito = params;
-      this.itemsCarrito = this.carritoService.itemsCarritoservice;
-      this.infoBack = this.carritoService.ultimaCompra;
-      this.infoQr = JSON.stringify(this.infoBack);
-    });
-    setTimeout(() => {
-      this.imprimirPDF();
-    }, 2000);
-  }
+    ngOnInit(): void {
+        this._route.queryParams.subscribe((params) => {
+            this.orderCarrito = params;
+            this.itemsCarrito = this.carritoService.itemsCarritoservice;
+            this.infoBack = this.carritoService.ultimaCompra;
+            this.infoQr = JSON.stringify(this.infoBack);
+        });
+        setTimeout(() => {
+            this.imprimirPDF();
+        }, 2000);
+    }
 
-  imprimirBoleta(): void {
-    const contenidoBoleta = this.elementRef.nativeElement.querySelector('#invoice-POS');
-    const elementosBody = Array.from(document.body.children) as HTMLElement[];
+    imprimirBoleta(): void {
+        const contenidoBoleta =
+            this.elementRef.nativeElement.querySelector('#invoice-POS');
+        const elementosBody = Array.from(
+            document.body.children
+        ) as HTMLElement[];
 
-    elementosBody.forEach((elemento: HTMLElement) => {
-      if (elemento !== contenidoBoleta) {
-        elemento.style.display = '';
-      }
-    });
+        elementosBody.forEach((elemento: HTMLElement) => {
+            if (elemento !== contenidoBoleta) {
+                elemento.style.display = '';
+            }
+        });
 
-    window.print();
+        window.print();
 
-    elementosBody.forEach((elemento: HTMLElement) => {
-      if (elemento !== contenidoBoleta) {
-        elemento.style.display = '';
-      }
-    });
-  }
+        elementosBody.forEach((elemento: HTMLElement) => {
+            if (elemento !== contenidoBoleta) {
+                elemento.style.display = '';
+            }
+        });
+    }
 
-//   imprimirPDF(): void {
-//   const contenidoBoleta = this.elementRef.nativeElement.querySelector('#invoice-POS');
+    //    imprimirPDF(): void {
+    //    const contenidoBoleta = this.elementRef.nativeElement.querySelector('#invoice-POS');
 
-//   html2canvas(contenidoBoleta, { scrollX: 0, scrollY: -window.scrollY, scale: 5 }).then(canvas => {
-//     const imgData = canvas.toDataURL('image/png');
-//     const doc = new jsPDF({
-//       unit: 'mm',
-//       format: [80, 297] // Ajusta las dimensiones según el tamaño de tu boleta POS 59 en milímetros
-//     });
+    //    html2canvas(contenidoBoleta, { scrollX: 0, scrollY: -window.scrollY, scale: 5 }).then(canvas => {
+    //      const imgData = canvas.toDataURL('image/png');
+    //      const doc = new jsPDF({
+    //        unit: 'mm',
+    //        format: [80, 297] // Ajusta las dimensiones según el tamaño de tu boleta POS 59 en milímetros
+    //      });
 
-//     const imgProps = doc.getImageProperties(imgData);
-//     const pdfWidth = doc.internal.pageSize.getWidth();
-//     const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    //      const imgProps = doc.getImageProperties(imgData);
+    //      const pdfWidth = doc.internal.pageSize.getWidth();
+    //      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-//     // Agregar imagen
-//     doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    //      // Agregar imagen
+    //      doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
 
-//     // Envía el archivo PDF a imprimir
-//     doc.autoPrint();
-//     const blob = doc.output('blob');
-//     const url = URL.createObjectURL(blob);
-//     window.open(url, '_blank');
+    //      // Envía el archivo PDF a imprimir
+    //      doc.autoPrint();
+    //      const blob = doc.output('blob');
+    //      const url = URL.createObjectURL(blob);
+    //      window.open(url, '_blank', );
 
-//     // Liberar recursos después de imprimir
-//     URL.revokeObjectURL(url);
-//   });
-// }
-imprimirPDF(): void {
-  const contenidoBoleta = this.elementRef.nativeElement.querySelector('#invoice-POS');
+    //      // Liberar recursos después de imprimir
+    //      URL.revokeObjectURL(url);
+    //    });
+    //  }
 
-  // Convierto el contenido de la boleta en una imagen y luego la exporto a pdf
-  html2canvas(contenidoBoleta, { scrollX: 0, scrollY: -window.scrollY, scale: 5 }).then(canvas => {
-    const imgData = canvas.toDataURL('image/png');
-    const doc = new jsPDF({
-      unit: 'mm',
-      format: [80, 297] 
-    });
-    //Configuro la imagen segun el tamano necesario
-    const imgProps = doc.getImageProperties(imgData);
-    const pdfWidth = doc.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    imprimirPDF(): void {
+        const contenidoBoleta =
+            this.elementRef.nativeElement.querySelector('#invoice-POS');
 
-    // exporto la  imagen a pdf 
-    doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        // Convierto el contenido de la boleta en una imagen y luego la exporto a pdf
+        html2canvas(contenidoBoleta, {
+            scrollX: 0,
+            scrollY: -window.scrollY,
+            scale: 10,
+        }).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const doc = new jsPDF({
+                unit: 'mm',
+                format: [80, 297],
+            });
+            //Configuro la imagen segun el tamano necesario
+            const imgProps = doc.getImageProperties(imgData);
+            const pdfWidth = doc.internal.pageSize.getWidth();
+            const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+            // exporto la  imagen a pdf
+            doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
 
-    // Generar el archivo PDF y luego imprimirlo usando print-js
-    const blob = doc.output('blob');
-    const url = URL.createObjectURL(blob);
-    printJS(url);
+            // Generar el archivo PDF y luego imprimirlo usando print-js
+            doc.autoPrint();
+            const blob = doc.output('blob');
+            const url = URL.createObjectURL(blob);
+            printJS(url);
 
-    // Limpia los recursos después de imprimir
-    URL.revokeObjectURL(url);
+            // Limpia los recursos después de imprimir
+            URL.revokeObjectURL(url);
 
-    // Recargar la página después de 10 segundos
-    setTimeout(() => {
-      window.location.reload();
-    }, 6000);
-  });
-}
+            // Recargar la página después de 10 segundos
+            setTimeout(() => {
+                window.location.reload();
+            }, 6000);
+        });
+    }
 }
 
 // imprimirPDF(): void {
@@ -144,8 +154,8 @@ imprimirPDF(): void {
 //       printWindow.addEventListener('afterprint', () => {
 //         printWindow.close();
 //         URL.revokeObjectURL(url);
-        
+
 //       });
 //     }
 //   });
-// }
+//
